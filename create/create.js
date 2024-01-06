b0b3be8663b8a2fc8c9cd4dd4b0faa7f8d53d835
xiumume <155123657+xiumume@users.noreply.github.com>
2024-01-06 17:47:50 +0800

    2024.01.06

---
+++ create/create.js
@@ -0,0 +1,11 @@
+function create() {
+    var print = document.getElementById("print");
+    var text = document.getElementById("val").value;
+    var lines = text.split("\n");
+    let objectsArray = lines.map(line => {
+        return { "name": line };
+    });
+    var endvalue = objectsArray;
+    print.value=JSON.stringify(endvalue);
+    console.log(endvalue);
+}
